dde7d7a8e6b3c34e085f81668c9b0c2f7331a76d
Mazdak Momen <mazdak.momen@gmail.com>
2014-12-18 16:44:13 -0800

    Added backend logic for comments (model\table created), comments are disabled if exists on show page. Future task is to add comments to the front end (ability to add\edit).

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,5 @@
 class Video < ActiveRecord::Base
   validates :title, presence: true
   validates :youtube_id, presence: true
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141218215354) do
+ActiveRecord::Schema.define(version: 20141219002143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.text     "body"
+    t.integer  "video_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "videos", force: true do |t|
     t.string   "title"
